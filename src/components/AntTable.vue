17da2e99f84da2f5c24cdca3717b5060acf4cee0
mwimer1 <macrae@getcorso.com>
2024-07-16 04:41:59 -0500

    Add files via upload

---
+++ src/components/AntTable.vue
@@ -0,0 +1,15 @@
+<template>
+    <Demo />
+  </template>
+  
+  <script>
+  import { defineComponent } from "vue";
+  import Demo from "./components/AntTable.vue";
+  
+  export default defineComponent({
+    components: {
+      Demo,
+    },
+  });
+  </script>
+  
